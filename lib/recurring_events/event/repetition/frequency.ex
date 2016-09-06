d09edf8e97dd98e3de06d5195f867f3ee2718e8e
Kamil Lelonek <squixy.sln@gmail.com>
2016-09-06 22:14:05 +0200

    Add and test frequency

---
+++ lib/recurring_events/event/repetition/frequency.ex
@@ -0,0 +1,69 @@
+defmodule RecurringEvents.Event.Repetition.Frequency do
+  use Exnumerator,
+    values: [
+      :once,
+      :daily,
+      :weekly,
+      :monthly,
+      :yearly
+    ]
+
+  def stream_of_dates(:once,     _interval, date_start, _date_end), do: [date_start]
+  def stream_of_dates(frequency,  interval, date_start,  date_end)  do
+    date_start
+    |> generate_dates(frequency, interval)
+    |> pick_dates_within_range(date_end)
+  end
+
+  defp generate_dates(date_start, frequency, interval),
+    do: Stream.iterate(date_start, &stepper(frequency, interval, &1))
+
+  defp pick_dates_within_range(dates_stream, date_end),
+    do: Stream.take_while(dates_stream, &not_future_date?(&1, date_end))
+
+  defp not_future_date?(date, boundary),
+    do: Ecto.Date.compare(date, boundary) != :gt
+
+  defp stepper(:daily,   interval, date), do: add_days(date,   interval)
+  defp stepper(:weekly,  interval, date), do: add_days(date,   interval * 7)
+  defp stepper(:monthly, interval, date), do: add_months(date, interval)
+  defp stepper(:yearly,  interval, date), do: add_months(date, interval * 12)
+
+  def add_days(date, days) do
+    date
+    |> Ecto.Date.to_erl()
+    |> :calendar.date_to_gregorian_days()
+    |> Kernel.+(days)
+    |> :calendar.gregorian_days_to_date()
+    |> Ecto.Date.from_erl()
+  end
+
+  def add_months(date, months) do
+    date
+    |> calculate_year_and_month(months)
+    |> adjust_year_and_month()
+    |> adjust_day()
+  end
+
+  defp calculate_year_and_month(%Ecto.Date{year: year, month: month, day: day}, months),
+    do: %Ecto.Date{year: calculate_year(year, months), month: calculate_month(month, months), day: day}
+
+  defp calculate_year(year, months),
+    do: year + div(months, 12)
+
+  defp calculate_month(month, months),
+    do: month + rem(months, 12)
+
+  defp adjust_year_and_month(%Ecto.Date{year: year, month: month, day: day}) when month < 1,
+    do: %Ecto.Date{year: year - 1, month: month + 12, day: day}
+  defp adjust_year_and_month(%Ecto.Date{year: year, month: month, day: day}) when month > 12,
+    do: %Ecto.Date{year: year + 1, month: month - 12, day: day}
+  defp adjust_year_and_month(date),
+    do: date
+
+  defp adjust_day(%Ecto.Date{year: year, month: month, day: _day} = date),
+    do: %Ecto.Date{year: year, month: month, day: calculate_day(date)}
+
+  defp calculate_day(%Ecto.Date{year: year, month: month, day: day}),
+    do: Enum.min([day, :calendar.last_day_of_the_month(year, month)])
+end
---
+++ test/event/repetition/frequency_test.exs
@@ -0,0 +1,106 @@
+defmodule RecurringEvents.Event.Repetition.FrequencyTest do
+  use ExUnit.Case, async: true
+
+  alias RecurringEvents.Event.Repetition.Frequency
+
+  describe "add_days" do
+    test "should add a couple of days and change a month",
+      do: assert %Ecto.Date{year: 2015, month: 3, day: 2} == Frequency.add_days(%Ecto.Date{year: 2015, month: 2, day: 27}, 3)
+
+    test "should add a couple of days and change a year",
+      do: assert %Ecto.Date{year: 2016, month: 1, day: 1} == Frequency.add_days(%Ecto.Date{year: 2015, month: 12, day: 30}, 2)
+
+    test "should subtract a couple of days within a month",
+      do: assert %Ecto.Date{year: 2015, month: 2, day: 24} == Frequency.add_days(%Ecto.Date{year: 2015, month: 2, day: 27}, -3)
+  end
+
+  describe "add_months" do
+    @last_jan_2015 %Ecto.Date{year: 2015, month: 1, day: 31}
+
+    test "should add a month and change to a shorter one" do
+      assert %Ecto.Date{year: 2015, month: 2, day: 28} == Frequency.add_months(@last_jan_2015, 1)
+    end
+
+    test "should add months and change to the next year" do
+      assert %Ecto.Date{year: 2016, month: 1, day: 31} == Frequency.add_months(@last_jan_2015, 12)
+    end
+
+    test "should pick the correct last day of the month the next year" do
+      assert %Ecto.Date{year: 2016, month: 2, day: 29} == Frequency.add_months(@last_jan_2015, 13)
+    end
+  end
+
+  describe "stream_of_dates" do
+    test "should generate only one date for :once frequency" do
+      frequency  = :once
+      interval   = nil
+      date_start = %Ecto.Date{year: 2000, month: 1, day: 1}
+      date_end   = %Ecto.Date{year: 3000, month: 1, day: 1}
+
+      assert_stream_of_dates(frequency, interval, date_start, date_end, [date_start])
+    end
+
+    test "should generate a stream of dates for every day" do
+      frequency  = :daily
+      interval   = 1
+      date_start = %Ecto.Date{year: 2000, month: 1, day: 1}
+      date_end   = %Ecto.Date{year: 2000, month: 1, day: 3}
+      dates      = [
+        %Ecto.Date{year: 2000, month: 1, day: 1},
+        %Ecto.Date{year: 2000, month: 1, day: 2},
+        %Ecto.Date{year: 2000, month: 1, day: 3},
+      ]
+
+      assert_stream_of_dates(frequency, interval, date_start, date_end, dates)
+    end
+
+    test "should generate a stream of dates for every second week" do
+      frequency  = :weekly
+      interval   = 2
+      date_start = %Ecto.Date{year: 2000, month: 1, day:  1}
+      date_end   = %Ecto.Date{year: 2000, month: 1, day: 31}
+      dates      = [
+        %Ecto.Date{year: 2000, month: 1, day:  1},
+        %Ecto.Date{year: 2000, month: 1, day: 15},
+        %Ecto.Date{year: 2000, month: 1, day: 29},
+      ]
+
+      assert_stream_of_dates(frequency, interval, date_start, date_end, dates)
+    end
+
+    test "should generate a stream of dates for every third month" do
+      frequency  = :monthly
+      interval   = 3
+      date_start = %Ecto.Date{year: 2000, month:  1, day:  1}
+      date_end   = %Ecto.Date{year: 2000, month: 12, day: 31}
+      dates      = [
+        %Ecto.Date{year: 2000, month:  1, day: 1},
+        %Ecto.Date{year: 2000, month:  4, day: 1},
+        %Ecto.Date{year: 2000, month:  7, day: 1},
+        %Ecto.Date{year: 2000, month: 10, day: 1},
+      ]
+
+      assert_stream_of_dates(frequency, interval, date_start, date_end, dates)
+    end
+
+    test "should generate a stream of dates for every fourth year" do
+      frequency  = :yearly
+      interval   = 4
+      date_start = %Ecto.Date{year: 2000, month: 1, day: 1}
+      date_end   = %Ecto.Date{year: 2020, month: 1, day: 1}
+      dates      = [
+        %Ecto.Date{year: 2000, month: 1, day: 1},
+        %Ecto.Date{year: 2004, month: 1, day: 1},
+        %Ecto.Date{year: 2008, month: 1, day: 1},
+        %Ecto.Date{year: 2012, month: 1, day: 1},
+        %Ecto.Date{year: 2016, month: 1, day: 1},
+        %Ecto.Date{year: 2020, month: 1, day: 1},
+      ]
+
+      assert_stream_of_dates(frequency, interval, date_start, date_end, dates)
+    end
+
+    defp assert_stream_of_dates(frequency, interval, date_start, date_end, dates),
+      do: assert dates == frequency |> Frequency.stream_of_dates(interval, date_start, date_end) |> Enum.to_list
+  end
+end
