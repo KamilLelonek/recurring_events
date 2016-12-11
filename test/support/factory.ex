a6fdf76a77e1baac76644d44611fab647c19a716
Kamil Lelonek <squixy.sln@gmail.com>
2016-12-11 12:11:42 +0100

    Add event to Factory

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,6 +1,7 @@
 defmodule RecurringEvents.Test.Factory do
   use ExMachina.Ecto, repo: RecurringEvents.Repo
 
+  alias RecurringEvents.Event
   alias RecurringEvents.Event.Repetition
 
   def repetition_factory() do
@@ -11,6 +12,13 @@ defmodule RecurringEvents.Test.Factory do
       end_date:   %Ecto.Date{day: 1, month: 2, year: 2000},
       start_time: %Ecto.Time{hour: 1, min: 0, sec: 0},
       end_time:   %Ecto.Time{hour: 2, min: 0, sec: 0}
+    }
+  end
+
+  def event_factory() do
+    %Event{
+      name:        sequence("Event Name"),
+      repetitions: build_list(1, :repetition)
     }
   end
 end
