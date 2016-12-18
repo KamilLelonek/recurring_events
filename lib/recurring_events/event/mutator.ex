902590fa4163a0ba71f410db25cec23e7fb25b18
Kamil Lelonek <squixy.sln@gmail.com>
2016-12-18 15:48:19 +0100

    Test Event deletion

--- lib/recurring_events/event/mutator.ex
+++ lib/recurring_events/event/mutator.ex
@@ -1,11 +1,20 @@
 defmodule RecurringEvents.Event.Mutator do
-  alias RecurringEvents.Event
+  import Ecto.Query
 
-  alias RecurringEvents.Repo
+  alias RecurringEvents.{Event, Repo}
 
   def create(params) do
     params
     |> Event.Changeset.build()
     |> Repo.insert()
   end
+
+  def delete(id) do
+    id
+    |> event_by_id_query()
+    |> Repo.delete_all()
+  end
+
+  defp event_by_id_query(id),
+    do: from e in Event, where: e.id == ^id
 end
--- test/event/mutator_test.exs
+++ test/event/mutator_test.exs
@@ -20,6 +20,32 @@ defmodule RecurringEvents.Event.MutatorTest do
     end
   end
 
+  describe "delete" do
+    test "should delete an existing event" do
+      %{id: id} = insert(:event)
+
+      assert 1      = count(:events)
+      assert {1, _} = Event.Mutator.delete(id)
+      assert 0      = count(:events)
+    end
+
+    test "should remove all Repetitions" do
+      %{id: id} = insert(:event)
+
+      assert 1      = count(:repetitions)
+      assert {1, _} = Event.Mutator.delete(id)
+      assert 0      = count(:repetitions)
+    end
+
+    test "should not delete a nonexistent event" do
+      assert {0, nil} = Event.Mutator.delete(Ecto.UUID.generate())
+    end
+  end
+
   defp event_from_repo(),
     do: Event |> Repo.one() |> Repo.preload(:repetitions)
+
+  defp count(:events),      do: count(Event)
+  defp count(:repetitions), do: count(Event.Repetition)
+  defp count(schema),       do: Repo.aggregate(schema, :count, :id)
 end
