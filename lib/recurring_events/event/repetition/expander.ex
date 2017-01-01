181fe3012c9397830f96ab30a91ecbdc3e23fee2
Kamil Lelonek <squixy.sln@gmail.com>
2017-01-01 19:01:58 +0100

    Don't require Repetition Schema for expanding

--- lib/recurring_events/event/repetition/expander.ex
+++ lib/recurring_events/event/repetition/expander.ex
@@ -1,7 +1,7 @@
 defmodule RecurringEvents.Event.Repetition.Expander do
   alias RecurringEvents.Event.Repetition
 
-  def stream(%Repetition{frequency: frequency, interval: interval, start_date: start_date, end_date: end_date, exclusions: exclusions}),
+  def stream(%{frequency: frequency, interval: interval, start_date: start_date, end_date: end_date, exclusions: exclusions}),
     do: Repetition.Frequency.stream_of_dates(frequency, interval, start_date, end_date, exclusions)
 end
 
