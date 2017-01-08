b2f9c87b6334f8afeebafa783452843ac440f849
Kamil Lelonek <squixy.sln@gmail.com>
2017-01-08 22:51:52 +0100

    Reduce number of occurrences in Factory

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -7,7 +7,7 @@ defmodule RecurringEvents.Test.Factory do
   def repetition_factory() do
     %Repetition{
       frequency:  :daily,
-      interval:   2,
+      interval:   10,
       start_date: %Ecto.Date{day: 1, month: 1, year: 2000},
       end_date:   %Ecto.Date{day: 1, month: 2, year: 2000},
       start_time: %Ecto.Time{hour: 1, min: 0, sec: 0},
