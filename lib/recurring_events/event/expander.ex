479d66fd268b9476a27d0f83c9cf08bc87325f32
Kamil Lelonek <squixy.sln@gmail.com>
2017-01-22 12:16:46 +0100

    Test Event Expander

--- lib/recurring_events/event/expander.ex
+++ lib/recurring_events/event/expander.ex
@@ -1,17 +1,22 @@
 defmodule RecurringEvents.Event.Expander do
   alias RecurringEvents.Event
 
-  def stream(%{id: id, name: name, repetitions: repetitions}) do
-    %Event{
-      name:        name,
-      repetitions: expand_repetitions(repetitions, id),
-    }
-  end
+  @number_of_occurences 30
 
-  defp expand_repetitions(repetitions, event_id),
-    do: Stream.flat_map(repetitions, &expand_repetition(&1, event_id))
+  def run(%{id: id, repetitions: repetitions} = event),
+    do: %Event{event | repetitions: expand_repetitions(repetitions, id)}
+
+  defp expand_repetitions(repetitions, event_id) do
+    repetitions
+    |> Stream.flat_map(&expand_repetition(&1, event_id))
+    |> Stream.reject(&past_occurence/1)
+    |> Stream.take(@number_of_occurences)
+    |> Enum.to_list()
+  end
 
   defp expand_repetition(repetition, event_id),
     do: Event.Repetition.Expander.stream(repetition, event_id)
-end
 
+  defp past_occurence(%Event.Repetition.Occurence{date: date}),
+    do: Ecto.Date.compare(date, Ecto.Date.utc) == :lt
+end
--- test/event/expander_test.exs
+++ test/event/expander_test.exs
@@ -5,7 +5,7 @@ defmodule RecurringEvents.Event.ExpanderTest do
   alias RecurringEvents.Event
 
   test "should expand all Events" do
-    %{repetitions: [%{id: repetition_id}]} = event = insert(:event)
+    %{repetitions: [%{id: repetition_id}]} = event = build(:event)
 
     %{repetitions: repetitions} = Event.Expander.run(event)
 
@@ -25,5 +25,23 @@ defmodule RecurringEvents.Event.ExpanderTest do
         time_end:      %Ecto.Time{hour: 1, min: 0, sec: 0},
       },
     ] == repetitions
+  end
+
+  test "should reject past Occurences" do
+    repetition = build(:repetition, %{start_date: %Ecto.Date{year: 2000, month: 1, day: 1}})
+    event      = build(:event, repetitions: [repetition])
+
+    %{repetitions: [%{date: first_repetition_date} | _]} = Event.Expander.run(event)
+
+    refute Ecto.Date.compare(first_repetition_date, Ecto.Date.utc) == :lt
+  end
+
+  test "should limit Occurences in the future" do
+    repetition = build(:repetition, %{end_date: %Ecto.Date{year: 3000, month: 1, day: 1}})
+    event      = build(:event, repetitions: [repetition])
+
+    %{repetitions: repetitions} = Event.Expander.run(event)
+
+    assert 30 = Enum.count(repetitions)
   end
 end
